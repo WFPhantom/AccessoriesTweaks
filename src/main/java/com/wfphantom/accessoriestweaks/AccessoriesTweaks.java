f792090e38aa408bfb245eb17b3963523fc7e10f
WFPhantom <wfphantom5@gmail.com>
2025-10-14 20:36:28 +0300

    make it client side only and add accessories dependency

--- src/main/java/com/wfphantom/accessoriestweaks/AccessoriesTweaks.java
+++ src/main/java/com/wfphantom/accessoriestweaks/AccessoriesTweaks.java
@@ -1,12 +1,12 @@
 package com.wfphantom.accessoriestweaks;
 
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.common.Mod;
 
 // TODO: Make this multiloader
 // TODO: Fix scroll working in the experimental menu due to mousetweaks
 // TODO: Move apothic attibutes button from vanilla menu to experiemental (Neo only)
-
-@Mod(AccessoriesTweaks.MODID)
+@Mod(value = AccessoriesTweaks.MODID, dist = Dist.CLIENT)
 public class AccessoriesTweaks {
     public static final String MODID = "accessoriestweaks";
 }
